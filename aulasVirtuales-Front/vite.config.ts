7a7d19ab0f54ffa03e6d46d7d4bca7cf0a25c688
Robert <paricar.dev@gmail.com>
2026-06-06 17:19:42 -0500

    Fix

--- aulasVirtuales-Front/vite.config.ts
+++ aulasVirtuales-Front/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     host: true,
     allowedHosts: [
-      "registrosacademicos.robertperez.site",
+      "api.registrosacademicos.robertperez.site",
       "localhost",
       "127.0.0.1",
     ],
