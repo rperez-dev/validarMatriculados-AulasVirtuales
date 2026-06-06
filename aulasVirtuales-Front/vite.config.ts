5128bb5fb8ed665893f882c300dbd2ba5e7885eb
Robert <paricar.dev@gmail.com>
2026-06-06 17:14:09 -0500

    fix cambios en frontend

--- aulasVirtuales-Front/vite.config.ts
+++ aulasVirtuales-Front/vite.config.ts
@@ -5,8 +5,11 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    host: "localhost",
-    port: 5173,
-    strictPort: true,
+    host: true,
+    allowedHosts: [
+      "registrosacademicos.robertperez.site",
+      "localhost",
+      "127.0.0.1",
+    ],
   },
 });
