92beba0bb00fefd0435a53a3ec091b60b7e3fd20
Robert <paricar.dev@gmail.com>
2026-06-07 09:51:54 -0500

    fix de vite config

--- aulasVirtuales-Front/vite.config.ts
+++ aulasVirtuales-Front/vite.config.ts
@@ -4,4 +4,13 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
+    allowedHosts: [
+      "registrosacademicos.robertperez.site",
+      "localhost",
+      "127.0.0.1",
+    ],
+  },
 });
